0efb0ec753d3dee75b86fdd3034cf82c5783ffc0
Chaitanya Chauhan <chaitanyachauhan@Chaitanyas-MacBook-Air-2.local>
2017-02-07 19:17:56 +0530

    pure js loading

--- js/loader.js
+++ js/loader.js
@@ -1,44 +1,44 @@
-// ;(function(){
-//   function id(v){return document.getElementById(v); }
-//   function loadbar() {
-//     var ovrl = id("loaderElevator"),
-//         stat = id("currentFloor"),
-//         leftDoor = id("leftDoor"),
-//         rightDoor = id("rightDoor"),
-//         loadingTechoMsg = id("loadingTechoMsg"),
-//         img = document.images,
-//         c = 0;
-//         //img = document.getElementsByTagName('*')
-//         tot = img.length;
-//         console.log(img);
-//
-//     function imgLoaded(){
-//       c += 1;
-//       var perc = ((100/tot*c) << 0);
-//       var floorNum = perc*24/100;
-//       stat.innerHTML = Math.ceil(floorNum);
-//       if(c===tot) return doneLoading();
-//     }
-//     function doneLoading(){
-//       setTimeout(function(){
-//         //ovrl.className += ' active';
-//         leftDoor.className += ' active';
-//         rightDoor.className += ' active';
-//         loadingTechoMsg.className += ' active';
-//       }, 500);
-//       // setTimeout(function(){
-//       //   ovrl.style.display = "none";
-//       // },4200);
-//     }
-//     for(var i=0; i<tot; i++) {
-//       var tImg     = new Image();
-//       tImg.onload  = imgLoaded;
-//       tImg.onerror = imgLoaded;
-//       tImg.src     = img[i].src;
-//     }
-//   }
-//   document.addEventListener('DOMContentLoaded', loadbar, false);
-// }());
+;(function(){
+  function id(v){return document.getElementById(v); }
+  function loadbar() {
+    var ovrl = id("loaderElevator"),
+        stat = id("currentFloor"),
+        leftDoor = id("leftDoor"),
+        rightDoor = id("rightDoor"),
+        loadingTechoMsg = id("loadingTechoMsg"),
+        img = document.images,
+        c = 0;
+        //img = document.getElementsByTagName('*')
+        tot = img.length;
+        console.log(img);
+
+    function imgLoaded(){
+      c += 1;
+      var perc = ((100/tot*c) << 0);
+      var floorNum = perc*24/100;
+      stat.innerHTML = Math.ceil(floorNum);
+      if(c===tot) return doneLoading();
+    }
+    function doneLoading(){
+      setTimeout(function(){
+        //ovrl.className += ' active';
+        leftDoor.className += ' active';
+        rightDoor.className += ' active';
+        loadingTechoMsg.className += ' active';
+      }, 500);
+      setTimeout(function(){
+        ovrl.style.display = "none";
+      },4200);
+    }
+    for(var i=0; i<tot; i++) {
+      var tImg     = new Image();
+      tImg.onload  = imgLoaded;
+      tImg.onerror = imgLoaded;
+      tImg.src     = img[i].src;
+    }
+  }
+  document.addEventListener('DOMContentLoaded', loadbar, false);
+}());
 
 
 // ;(function(){
@@ -96,78 +96,78 @@
 // }
 // }());
 
-;(function(){
-  function id(v){return document.getElementById(v); }
-  function loadbar() {
-        var ovrl = id("loaderElevator"),
-        stat = id("currentFloor"),
-        leftDoor = id("leftDoor"),
-        //loadingTechoMsg = id("loadingTechoMsg"),
-        rightDoor = id("rightDoor");
-        // img = document.images,
-        // c = 0;
-        // tot = img.length;
-        // console.log(img);
-
-    // function imgLoaded(){
-      // c += 1;
-      // var perc = ((100/tot*c) << 0);
-      // var floorNum = perc*24/100;
-      // stat.innerHTML = Math.ceil(floorNum);
-      // if(c===tot) return doneLoading();
-    // }
-    // function doneLoading(){
-      // setTimeout(function(){
-        // ovrl.className += ' active';
-        // leftDoor.className += ' active';
-        // rightDoor.className += ' active';
-      // }, 500);
-      // setTimeout(function(){
-        // ovrl.style.display = "none";
-      // },4200);
-    // }
-    // for(var i=0; i<tot; i++) {
-      // var tImg     = new Image();
-      // tImg.onload  = imgLoaded;
-      // tImg.onerror = imgLoaded;
-      // tImg.src     = img[i].src;
-    // }
-	var countImages = $('body img').length;
-	$('body').imagesLoaded()
-	  .always( function( instance ) {
-		console.log('all images loaded');
-    setTimeout(function(){
-			ovrl.className += ' active';
-			leftDoor.className += ' active';
-			rightDoor.className += ' active';
-      //loadingTechoMsg.className += ' active';
-		  }, 500);
-		  setTimeout(function(){
-			ovrl.style.display = "none";
-		  },4200);
-	  })
-	  .done( function( instance ) {
-		console.log('all images successfully loaded');
-
-	  })
-	  .fail( function() {
-		console.log('all images loaded, at least one is broken');
-	  })
-	  .progress( function( instance, image ) {
-		//var result = image.isLoaded ? 'loaded' : 'broken';
-		//console.log( 'image is ' + result + ' for ' + image.img.src );
-		if(image.isLoaded) {
-			$(image.img).addClass('loaded');
-
-			var countLoadedImages = $('body img.loaded').length;
-
-			var perc = 100 * (countLoadedImages / countImages);
-			//console.log(width)
-			var floorNum = perc*24/100;
-			console.log(Math.floor(floorNum))
-            stat.innerHTML = Math.ceil(floorNum);
-		  }
-	  });
-    }
-  document.addEventListener('DOMContentLoaded', loadbar, false);
-}());
+// ;(function(){
+//   function id(v){return document.getElementById(v); }
+//   function loadbar() {
+//         var ovrl = id("loaderElevator"),
+//         stat = id("currentFloor"),
+//         leftDoor = id("leftDoor"),
+//         //loadingTechoMsg = id("loadingTechoMsg"),
+//         rightDoor = id("rightDoor");
+//         // img = document.images,
+//         // c = 0;
+//         // tot = img.length;
+//         // console.log(img);
+//
+//     // function imgLoaded(){
+//       // c += 1;
+//       // var perc = ((100/tot*c) << 0);
+//       // var floorNum = perc*24/100;
+//       // stat.innerHTML = Math.ceil(floorNum);
+//       // if(c===tot) return doneLoading();
+//     // }
+//     // function doneLoading(){
+//       // setTimeout(function(){
+//         // ovrl.className += ' active';
+//         // leftDoor.className += ' active';
+//         // rightDoor.className += ' active';
+//       // }, 500);
+//       // setTimeout(function(){
+//         // ovrl.style.display = "none";
+//       // },4200);
+//     // }
+//     // for(var i=0; i<tot; i++) {
+//       // var tImg     = new Image();
+//       // tImg.onload  = imgLoaded;
+//       // tImg.onerror = imgLoaded;
+//       // tImg.src     = img[i].src;
+//     // }
+// 	var countImages = $('body img').length;
+// 	$('body').imagesLoaded()
+// 	  .always( function( instance ) {
+// 		console.log('all images loaded');
+//     setTimeout(function(){
+// 			ovrl.className += ' active';
+// 			leftDoor.className += ' active';
+// 			rightDoor.className += ' active';
+//       //loadingTechoMsg.className += ' active';
+// 		  }, 500);
+// 		  setTimeout(function(){
+// 			ovrl.style.display = "none";
+// 		  },4200);
+// 	  })
+// 	  .done( function( instance ) {
+// 		console.log('all images successfully loaded');
+//
+// 	  })
+// 	  .fail( function() {
+// 		console.log('all images loaded, at least one is broken');
+// 	  })
+// 	  .progress( function( instance, image ) {
+// 		//var result = image.isLoaded ? 'loaded' : 'broken';
+// 		//console.log( 'image is ' + result + ' for ' + image.img.src );
+// 		if(image.isLoaded) {
+// 			$(image.img).addClass('loaded');
+//
+// 			var countLoadedImages = $('body img.loaded').length;
+//
+// 			var perc = 100 * (countLoadedImages / countImages);
+// 			//console.log(width)
+// 			var floorNum = perc*24/100;
+// 			console.log(Math.floor(floorNum))
+//             stat.innerHTML = Math.ceil(floorNum);
+// 		  }
+// 	  });
+//     }
+//   document.addEventListener('DOMContentLoaded', loadbar, false);
+// }());
