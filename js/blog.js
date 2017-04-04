56a8e78ba74a675a14d3438a2778b9c171c96235
Chaitanya Chauhan <chaitanyachauhan@Chaitanyas-MacBook-Air-2.local>
2017-04-04 16:50:07 +0530

    blog related changes

--- js/blog.js
+++ js/blog.js
@@ -1,65 +1,58 @@
-$.ajax({
-    //url: "https://jsonplaceholder.typicode.com/posts/1/comments",
-    url: "http://dev.techolution.com:5051/api/techo_blogs_v1-0",
-    type: "GET",
+//  Create the XHR object.
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS not supported.
+        xhr = null;
+    }
+    return xhr;
+}
 
-    contentType: 'application/json; charset=utf-8',
-    success: function(resultData) {
-        console.log(resultData);
-        for (let i = 0; i < resultData.length; i++) {
-            console.log(resultData);
-            $('#ajax-body').append(`<p>${resultData[i]}</p>`);
+// Make the actual CORS request.
+(function() {
+    // This is a sample server that supports CORS.
+    var url = 'http://dev.techolution.com:5051/api/techo_blogs_v1-0';
+
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+        return;
+    }
+
+    // Response handlers.
+    xhr.onload = function(e) {
+        var response = JSON.parse(xhr.responseText);
+        if (window.location.href.indexOf('?q=') > -1) {
+            var blogSubject = window.location.href.split('?q=');
+            response.map(function(object) {
+                if (object.field_id[0].value == blogSubject[1]) {
+                    $('#ajax-title, #ajax-title-mobile').text(object.title[0].value);
+                    var bodyData = '';
+                    if (object.body[0].value.indexOf('/sites/default/files/') > -1) {
+                        console.log('in');
+                        bodyData = object.body[0].value.replace(/\/sites/gi, 'http://dev.techolution.com:5051/sites');
+                        $('#ajax-body').html(bodyData);
+                    } else {
+                        $('#ajax-body').html(object.body[0].value);
+                    }
+
+
+                }
+            });
+
+
         }
-    },
-    error: function(jqXHR, textStatus, errorThrown) {},
+    };
 
-    timeout: 120000,
-});
-var dataIdTitle = '';
+    xhr.onerror = function(e) {
+        e.preventDefault();
+    };
 
-// Create the XHR object.
-// function createCORSRequest(method, url) {
-//     var xhr = new XMLHttpRequest();
-//     if ("withCredentials" in xhr) {
-//         // XHR for Chrome/Firefox/Opera/Safari.
-//         xhr.open(method, url, true);
-//     } else if (typeof XDomainRequest != "undefined") {
-//         // XDomainRequest for IE.
-//         xhr = new XDomainRequest();
-//         xhr.open(method, url);
-//     } else {
-//         // CORS not supported.
-//         xhr = null;
-//     }
-//     return xhr;
-// }
-
-// // Helper method to parse the title tag from the response.
-// function getTitle(text) {
-//     return text.match('<title>(.*)?</title>')[1];
-// }
-
-// // Make the actual CORS request.
-// (function() {
-//     // This is a sample server that supports CORS.
-//     var url = 'http://dev.techolution.com:5051/api/techo_blogs_v1-0';
-
-//     var xhr = createCORSRequest('GET', url);
-//     if (!xhr) {
-//         alert('CORS not supported');
-//         return;
-//     }
-
-//     // Response handlers.
-//     xhr.onload = function() {
-//         var text = xhr.responseText;
-//         var title = getTitle(text);
-//         alert('Response from CORS request to ' + url + ': ' + title);
-//     };
-
-//     xhr.onerror = function() {
-//         alert('Woops, there was an error making the request.');
-//     };
-
-//     xhr.send();
-// })();
+    xhr.send();
+})();
